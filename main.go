580a2c378c189b2e9be70ca704d7cba28f9224d7
Josep Marcello <jspmarcello@live.com>
2022-08-13 19:54:01 +0700

    chore: init project

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+type Route struct {
+	Src string `form:"src"`
+	Dst string `form:"dst"`
+}
+
+func main() {
+	godotenv.Load()
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(418).SendString("hehe")
+	})
+	app.Post("/", func(c *fiber.Ctx) error {
+		r := new(Route)
+		if err := c.BodyParser(r); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+		return c.SendString(fmt.Sprintf("{\n\tsrc: \"%s\",\n\tdst: \"%s\"\n}", r.Src, r.Dst))
+	})
+	app.Get("/*", func(c *fiber.Ctx) error {
+		route := c.Params("*")
+		if route != "" {
+			return c.SendString(route)
+		}
+		return c.Status(400).SendString("bad route")
+	})
+	app.Listen(":3000")
+}
