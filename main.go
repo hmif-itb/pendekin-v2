1eaa24b35ed17a874e7d1729b5efe681dc9777c9
Josep Marcello <jspmarcello@live.com>
2022-08-14 19:28:14 +0700

    feat: add Go mongo driver

--- main.go
+++ main.go
@@ -14,6 +14,32 @@ type Route struct {
 
 func main() {
 	godotenv.Load()
+
+	// load and check env variables
+	mongoUri := os.Getenv("MONGO_URI")
+	mongoDb := os.Getenv("MONGO_DB")
+	if !strings.HasPrefix(mongoUri, "mongodb://") {
+		panic("Invalid MONGO_URI env var")
+	}
+	if mongoDb == "" {
+		panic("MONGO_DB env var have to defined")
+	}
+
+	// setup mongo
+	ctx := context.TODO()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	db := client.Database(mongoDb)
+	collectionRoutes := db.Collection("routes")
+
+	// setup web server
 	app := fiber.New()
 	app.Static("/", "./view/public")
 	app.Post("/", func(c *fiber.Ctx) error {
@@ -30,5 +56,7 @@ func main() {
 		}
 		return c.Status(400).SendString("bad route")
 	})
+
+	// start app
 	app.Listen(":3000")
 }
